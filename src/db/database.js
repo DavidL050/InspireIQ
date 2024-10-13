627071e5c7b9d0ec71e29d299d9ab4bfda632a43
MateoVR13 <mattvr130@gmail.com>
2024-10-12 20:00:28 -0500

    Se corrigió la posición de la barra de busqueda.

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345678',
+    password: '840862_Mv',
     database: 'inspire_iq'
 });
 
