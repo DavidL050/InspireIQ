b66795d78fe840e1e141e9c7decf775e17982d6d
MateoVR13 <mattvr130@gmail.com>
2024-11-10 11:09:33 -0500

    Se cambió la uri de la DB, se ajustaron las alturas de los formularios y se corrigieron estilos del index

--- src/db/database.js
+++ src/db/database.js
@@ -2,12 +2,8 @@ import mysql from 'mysql2/promise';  // Cambia a la versión promise
 
 // Crear una conexión utilizando Promesas
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '12345678',
-    database: 'inspire_iq'
+    uri: 'mysql://root:itxUoywNJeozixvuVIpadbyTfGpxcoil@autorack.proxy.rlwy.net:32235/railway'
 });
-
 
 
 export default connection;
