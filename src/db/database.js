4ea675e0e83b3ad7fa340b0387f704b3b2e463f5
David Leal <davidleal@Davids-MacBook-Pro.local>
2024-11-14 00:14:10 -0500

    package json conflicts

--- src/db/database.js
+++ src/db/database.js
@@ -2,8 +2,11 @@ import mysql from 'mysql2/promise';
 
 // Crear una conexión utilizando Promesas
 const connection = mysql.createPool({
-    uri: 'mysql://root:itxUoywNJeozixvuVIpadbyTfGpxcoil@autorack.proxy.rlwy.net:32235/railway'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
-
 
 export default connection;
